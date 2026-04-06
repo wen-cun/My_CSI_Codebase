c76cc1403bcea5d4cff6cb6f45594cd23f248691
Wang Jiakuo <jkw_0211@163.com>
2026-04-06 23:22:11 +0800

    目前支持 常用、多峰两种类型的光源

--- Basic_Function/Source/demo_gen_lightsource.m
+++ Basic_Function/Source/demo_gen_lightsource.m
@@ -3,7 +3,7 @@ clear;
 close all;
 clc;
 %% 定义输入变量和输出变量
-type=1;%输入变量：光源类型
+type=2;%输入变量：光源类型
 vk0=1./(0.3:0.001:0.8)';%输入变量：查询点波数
 
 % vsk=nan*ones(size(vk0));%输出变量：查询点强度
@@ -11,6 +11,8 @@ vk0=1./(0.3:0.001:0.8)';%输入变量：查询点波数
 switch type
     case 1
         vsk=expe_usual(vk0);
+    case 2
+        vsk=expe_many_peaks(vk0);
     otherwise
         error('光源类型不支持！！！');
 end
