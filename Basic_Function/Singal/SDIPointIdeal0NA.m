46683bbdc260e5cdb33c1bddf1acd381b49afa0a
Wang Jiakuo <jkw_0211@163.com>
2026-06-16 21:38:28 +0800

    计算SDI 理想偏振0NA信号的函数

---
+++ Basic_Function/Singal/SDIPointIdeal0NA.m
@@ -0,0 +1,31 @@
+function signal=SDIPointIdeal0NA(vk0,vsk,A1,A2,sample_dis)
+% 这个函数用来计算理想偏振偏振NA=0下的点式SDI信号
+
+% 输入vk0为光谱采样点波数, double型N×1维向量，N为查询点个数
+% 输入vsk为采样点强度（等波数空间）, double型N×1维向量，N为查询点个数
+% 输入A1为样品振幅反射率, complex double型1×1维向量
+% 输入A2为参考镜振幅反射率, complex double型1×1维向量
+% 输入sample_dis为样品与参考镜的距离, double型1×1维向量
+
+%输出signal为SDI信号， double 型N×1维向量
+
+%     例：
+%     lam_peri = 3e-4; %波长采样周期,um制
+%     lam_lim = [0.3,1.1]; %波长采样范围
+%     lam = lam_lim(1):lam_peri:lam_lim(2); %波长采样
+%     lam = lam(:); %转为列向量
+%     vk0 = 1./lam; %转为波数
+%     vsk_ini = gen_lightsource(vk0,1);%获取光源信号
+%     vsk = vsk_ini./(vk0.^2);%波长波数域转换
+%     vsk=vsk./(max(abs(vsk)));
+%     A1=1;
+%     A2=2;
+%     sample_dis=3;
+%     signal=SDIPointIdeal0NA(vk0,vsk,A1,A2,sample_dis)
+
+vk0_mat = vk0; %光谱数据保持不变
+inter=abs(A1)^2+abs(A2)^2+2*real(A1*conj(A2)*exp(1i*4*pi.*vk0_mat.*sample_dis));
+signal = vsk.*inter; %光源直接乘以调制项
+signal = signal./max(abs(signal),[],'all'); %信号归一化
+end
+
