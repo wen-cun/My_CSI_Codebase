4d0e8a7ccce3e5e3d28ee8a99772133353e6ae9e
Wang Jiakuo <jkw_0211@163.com>
2026-07-03 18:07:58 +0800

    增加Valid参数，增加负半轴搜索的部分

--- Basic_Function/Processing/SDIPointModelFit.m
+++ Basic_Function/Processing/SDIPointModelFit.m
@@ -1,9 +1,9 @@
-function [z_pre,cost_pre]=SDIPointModelFit(signal,z_coa,rsquare,NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,theta_array,system_pol)
+function [z_pre,cost_pre]=SDIPointModelFit(signal,z_coa,valid,NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,theta_array,system_pol)
 % 这个函数用精细网格给出SDI信号位移值的精确定位结果
 
 % 输入 signal 为要匹配的目标信号，double型 N×1维向量
 % 输入 z_coa 为粗定位的结果，double型1×1维向量
-% 输入 rsquare为粗定位结果的置信程度，double型1×1维向量，当rsquare<0.75时将先采用粗网格修正粗定位结果
+% 输入 valid 为计算信号内部的有效位置逻辑数组，Logical型N×1维向量
 % 输入vk0为光谱采样点波数, double型N×1维向量，N为查询点个数
 % 输入vsk为采样点强度（等波数空间）, double型N×1维向量，N为查询点个数
 % 输入r_Se为样品TE偏振反射率, complex double型N×M维向量，N为光谱采样点个数,M为角度序列的长度
@@ -35,64 +35,60 @@ function [z_pre,cost_pre]=SDIPointModelFit(signal,z_coa,rsquare,NA,vk0,vsk,r_Se,
 %     [r_Me,r_Mm]=CalcMirrorAmplitudeReflectivity(vk0,theta_array);
 %     system_pol='ideal';
 %     signal=SDIPointSignalGenerate(NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,theta_array,3,system_pol));
-%     [z_coa,rsquare]=SDIPointModulFit(signal,lam,vsk_ini,NA);
-%     [z_pre,cost_pre]=SDIPointModelFit(signal,z_coa,rsquare,NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,theta_array,system_pol)
+%     source_thr = 0.05; %仅选取光源强度在最大值0.05以上的值进行粗略定位，以降低噪声
+%     valid = vsk_ini > source_thr*max(vsk_ini);
+%     z_coa=SDIPointModulFit(signal,lam,vsk_ini,valid,NA);
+%     [z_pre,cost_pre]=SDIPointModelFit(signal,z_coa,valid,NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,theta_array,system_pol)
 
-if rsquare<=0.75
-    warning('粗定位效果较差，改用粗细网格搜索法精确定位');
-    
+  
     z_add = 2.5; %扫描的上范围
     z_min = max(z_coa-z_add,0); %搜索的下限
     z_max = z_coa+z_add; %搜索的上限
     z_peri = 5e-2; %50nm采样
     z_gra = z_min:z_peri:z_max; %搜索的区间
     
-    cost=CalcSeekCost(NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,theta_array,system_pol,z_gra,signal);
+    cost_pos=CalcSeekCost(NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,theta_array,system_pol,z_gra,signal,valid);
     
-    [~,index]=min(cost);
-    z_coa=z_gra(index); %粗网格修正粗定位结果
-end
-
-z_add = 0.5; %扫描的上下范围
-z_min = max(z_coa-z_add,0); %搜索的下限
-z_max = z_coa+z_add; %搜索的上限
-z_peri = 1e-3; %1nm采样
-z_gra = z_min:z_peri:z_max; %搜索的区间
-
-cost=CalcSeekCost(NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,theta_array,system_pol,z_gra,signal);
-
-[~,min_index] = min(cost); %误差最小值对应的索引
-z_pre = z_gra(min_index);
-cost_pre = cost(min_index);
-
-if min_index == 1 || min_index == length(z_gra)
-    warning('误差最小值位于搜索边界，建议扩大 搜索范围');
-else
-    %抛物线三点拟合
-    % 最小值左右相邻的三个代价函数值
-    cost_left   = cost(min_index-1);
-    cost_center = cost(min_index);
-    cost_right  = cost(min_index+1);
+    [~,index]=min(cost_pos);
+    z_coa_pos=z_gra(index); %粗网格正半轴修正结果
     
-    denominator = cost_left - 2*cost_center + cost_right;
+    z_minus_min = -z_coa-z_add; %搜索负半部分
+    z_minus_max = min(-z_coa+z_add,0); %搜索负半部分
+    z_gra = z_minus_min:z_peri:z_minus_max; %搜索的区间
     
-    % denominator>0 表示局部抛物线开口向上
-    if isfinite(denominator) && denominator > eps(max(abs(cost)))
-        delta_index = 0.5 * ...
-            (cost_left - cost_right) / denominator;
-        
-        % 顶点原则上应该落在相邻两个采样点之间
-        if abs(delta_index) <= 1
-            z_pre = z_gra(min_index) + delta_index*z_peri;
-            
-            % 抛物线顶点对应的代价值
-            cost_pre = cost_center ...
-                - (cost_left-cost_right)^2/(8*denominator);
-        else
-            warning('抛物线顶点超出相邻采样区间，使用离散最小值。');
-        end
+    cost_neg=CalcSeekCost(NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,theta_array,system_pol,z_gra,signal,valid);
+    
+    [~,index]=min(cost_neg);
+    z_coa_neg=z_gra(index); %粗网格负半轴修正结果
+    
+    z_add_fine = 0.25;
+    z_step_fine = 1e-3;
+    
+    % 正半轴细网格
+    z_pos_min = max(z_coa_pos-z_add_fine,0);
+    z_pos_max = z_coa_pos+z_add_fine;
+    z_fine_pos = z_pos_min:z_step_fine:z_pos_max;
+    cost_fine_pos=CalcSeekCost(NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,theta_array,system_pol,z_fine_pos,signal,valid);
+    
+    
+    % 负半轴细网格
+    z_neg_min = z_coa_neg-z_add_fine;
+    z_neg_max = min(z_coa_neg+z_add_fine,0);
+    z_fine_neg = z_neg_min:z_step_fine:z_neg_max;
+    cost_fine_neg=CalcSeekCost(NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,theta_array,system_pol,z_fine_neg,signal,valid);
+    
+    [z_pos_refined,cost_pos_refined,~] = ...
+        RefineMinimumByParabola(z_fine_pos,cost_fine_pos);
+    
+    [z_neg_refined,cost_neg_refined,~] = ...
+        RefineMinimumByParabola(z_fine_neg,cost_fine_neg);
+    
+    if cost_pos_refined <= cost_neg_refined
+        z_pre = z_pos_refined;
+        cost_pre = cost_pos_refined;
     else
-        warning('局部代价函数不满足开口向上的抛物线条件，使用离散最小值。');
+        z_pre = z_neg_refined;
+        cost_pre = cost_neg_refined;
     end
-end
+    
 end
