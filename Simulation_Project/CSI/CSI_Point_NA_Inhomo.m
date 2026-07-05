1bb9ec6c337e659c6b3eb17cded664e663483ab1
Wang Jiakuo <jkw_0211@163.com>
2026-07-05 11:23:54 +0800

    CSI非均匀采样信号处理

---
+++ Simulation_Project/CSI/CSI_Point_NA_Inhomo.m
@@ -0,0 +1,264 @@
+%% 这个脚本用来解决非均匀采样CSI信号处理的问题
+clear; 
+close all;
+clc;
+%% 生成扫描序列
+d_peri=0.0719;%PZT单次移动间隔，um制
+% z_samp=0.05;%PZT单次移动间隔，um制
+
+N_half = round(10 / d_peri); % 计算单侧点数
+z_scan_pur = (-N_half : N_half) * d_peri; %不带振动的纯净扫描序列
+
+t_peri = 1e-3; %时间采样
+t = (0:length(z_scan_pur)-1)*t_peri;
+
+vib_freq_hz = [100,200];
+vib_amp = [0.2,0.1];
+
+vib = zeros(size(t));
+for ii = 1:numel(vib_freq_hz)
+    vib = vib + vib_amp(ii).*cos(2*pi*vib_freq_hz(ii).*t);
+end
+
+z_scan = z_scan_pur + vib; %扫描序列加上振动量
+
+figure();
+plot(t,vib,'LineWidth',1.5,'Color',GetColor(1,1));
+defaultAxes(2);
+xlabel('x/$\mu$m','Interpreter','latex');
+
+Color=GetColor(2,1);
+figure();
+plot(z_scan_pur,z_scan_pur,'LineWidth',1.5,'Color',Color(1,:));
+hold on;
+plot(z_scan_pur,z_scan,'-','LineWidth',1.5,'Color',Color(2,:));
+hold off;
+legend('Pur','Vib','EdgeColor','none','Location','NorthWest');
+defaultAxes(2);
+xlabel('x/$\mu$m','Interpreter','latex');
+
+vib_spe = fftshift(fft(vib)); %振动的频谱
+vib_spe = vib_spe./max(abs(vib_spe));
+vib_fre=make_axis_freq(length(t),t_peri,'101');
+vib_pow = abs(vib_spe).^2; %振动的功率谱
+vib_val = vib_fre >= 0;
+
+figure();
+plot(vib_fre(vib_val),vib_pow(vib_val),'LineWidth',1.5,'Color',GetColor(1,1));
+defaultAxes(2);
+xlabel('x/Hz');
+%% 生成光源
+fre = make_axis_freq(length(z_scan),d_peri,'101');
+fre = fre(fre> 2/1.1);%只选取大于1.1的部分；
+vk0 = (fre /2)';  %仿真中仍采用该顺序生成光源采样点波数
+
+vsk_ini = gen_lightsource(vk0,1);%获取光源信号
+vsk = vsk_ini./(vk0.^2); %波长波数域转换
+vsk = vsk./(max(abs(vsk))); %归一化光谱
+
+figure();
+tiledlayout(3,1);
+
+nexttile;
+plot(1./vk0,vsk_ini,'LineWidth',1.5,'Color',GetColor(1,1));
+defaultAxes(2);
+xlabel('$\lambda$/$\mu$m','Interpreter','latex');
+
+nexttile;
+plot(vk0,vsk_ini,'LineWidth',1.5,'Color',GetColor(1,1));
+defaultAxes(2);
+xlabel('k/$\mu m^{-1}$','Interpreter','latex');
+
+nexttile;
+plot(vk0,vsk,'LineWidth',1.5,'Color',GetColor(1,1));
+defaultAxes(2);
+xlabel('k/$\mu m^{-1}$','Interpreter','latex');
+%% 系统参数
+NA = 0.3; %系统NA
+theta_max=asin(NA); %最大NA对应的空气中光线角度theta
+theta_peri=0.01; %角度theta的采样周期
+theta_array = 0:theta_peri:theta_max; %theta采样数组
+%% 样品反射率
+% sample_stru = {'SiO2',5;...
+%                'Si',inf}; %样品结构
+sample_stru = {'Si',inf}; %样品结构
+[r_Se,r_Sm] = CalcSampleAmplitudeReflectivity(vk0,theta_array,sample_stru); %计算样品TE场、TM场反射率
+
+% figure();
+% surf(repmat(vk0,1,length(theta_array)),repmat(theta_array,length(vk0),1),abs(r_Se).^2);
+% shading interp;
+% defaultAxes(3);
+% defaultColor(1);
+% xlabel('k/$\mu m^{-1}$','Interpreter','latex');
+% ylabel('$\theta$/rad','Interpreter','latex');
+
+%% 参考镜反射率
+[r_Me,r_Mm] = CalcMirrorAmplitudeReflectivity(vk0,theta_array); %计算参考镜TE场、TM场反射率
+
+% figure();
+% surf(repmat(vk0,1,length(theta_array)),repmat(theta_array,length(vk0),1),abs(r_Me).^2);
+% shading interp;
+% defaultAxes(3);
+% defaultColor(1);
+% xlabel('k/$\mu m^{-1}$','Interpreter','latex');
+% ylabel('$\theta$/rad','Interpreter','latex');
+%% 选择偏振模式，生成白光干涉信号
+system_pol = 'unpolar';%非偏振模式
+% system_pol = 'ideal';%理想偏振模式
+
+sample_dis = 0;%样品与参考镜之间的距离;
+
+signal = CSIPointSignalGenerate(NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,z_scan,theta_array,sample_dis,system_pol);%生成CSI信号
+
+figure();
+plot(z_scan_pur,signal,'LineWidth',1.5,'Color',GetColor(1,1));
+defaultAxes(2);
+xlabel('z/$\mu m$','Interpreter','latex');
+
+%信号加高斯白噪声
+SNR = 40; %40dB的噪声
+signal = awgn(signal,SNR,'measured');
+signal = signal/max(abs(signal)); %归一化
+
+figure();
+plot(z_scan_pur,signal,'LineWidth',1.5,'Color',GetColor(1,1));
+defaultAxes(2);
+xlabel('z/$\mu m$','Interpreter','latex');
+%% 仅恢复部分扫描序列
+z_thr = 0.5; %在与0距离为z_thr的序列不完全可靠
+z_scan_un = z_scan; %不可靠的序列
+val = abs(z_scan_un)<z_thr;
+% z_scan_un(val) = z_scan_un(val) + 0.01*rand(1,sum(val)); %带部分噪声
+z_scan_un(val) = nan; %全为nan值
+
+% z_bol = rand(size(z_scan_un(val)));
+% z_bol(z_bol <= 0.5) = -1;
+% z_bol(z_bol > 0.5) = 1;
+% z_scan_un(val) = z_scan_un(val).*z_bol; %随机符号模糊
+% 
+% figure();
+% scatter(z_scan,z_scan_un,25,GetColor(1,1),'filled');
+% defaultAxes(2);
+% xlabel('z/$\mu$m','Interpreter','latex');
+
+ 
+%% 粗定位 能量重心法
+
+
+signal = signal(:);
+signal = signal-mean(signal); %去掉直流
+
+z_scan_un = z_scan_un(:);
+valid_z = isfinite(z_scan_un);
+
+energy = signal.^2;
+
+d_coa = sum(energy(valid_z).*z_scan_un(valid_z)) ...
+      / sum(energy(valid_z));
+  
+% d_coa = sum((signal.^2.*z_scan_un'),'omitnan')./sum(signal.^2,'omitnan');
+
+z_scan_un=z_scan_un';
+
+figure();
+scatter(z_scan_un,signal,25,Color(1,:),'filled');
+hold on;
+scatter(d_coa,0,25,Color(2,:),'filled');
+hold off;
+defaultAxes(2);
+legend('Signal','Center','EdgeColor','none');
+xlabel('z/$\mu m$','Interpreter','latex');
+
+
+
+disp(['预设高度h:',num2str(sample_dis)]);
+disp(['粗定位法:',num2str(d_coa)]);
+
+%%  精确定位 模型拟合
+% 粗定位误差控制在正负2um内
+% 粗细网格结合法
+d_add = 2; %多扫描的范围
+d_peri = 5e-2; %采样周期
+d_min = d_coa-d_add;
+d_max = d_coa+d_add;
+d_gra = d_min : d_peri : d_max; %搜索范围
+cost = nan*ones(size(d_gra)); %预先分配内存
+tic;
+for ii=1:length(d_gra)
+    signal_gra = CSIPointSignalGenerate(NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,z_scan_un,theta_array,d_gra(ii),system_pol);%生成CSI信号
+    signal_gra = signal_gra(:);
+    signal_gra = signal_gra-mean(signal_gra,'omitnan'); %去掉直流
+    cost(ii) = sum(abs(signal-signal_gra).^2,'all','omitnan');%计算误差
+end
+toc;
+[~,index]=min(cost);
+figure();
+plot(d_gra,cost,'LineWidth',1.5,'Color',Color(1,:));
+hold on;
+scatter(d_gra(index),cost(index),25,Color(2,:),'filled');
+hold off;
+defaultAxes(2);
+xlabel('z/$\mu$ m','Interpreter','latex');
+d_coa=d_gra(index); %粗网格修正粗定位结果
+disp(['粗网格拟合法:',num2str(d_coa)]);
+
+d_add = 0.25; %多扫描的范围
+d_peri = 1e-3; %采样周期
+d_min = d_coa-d_add;
+d_max = d_coa+d_add;
+d_gra = d_min : d_peri : d_max; %搜索范围
+cost = nan*ones(size(d_gra)); %预先分配内存
+tic;
+for ii=1:length(d_gra)
+    signal_gra = CSIPointSignalGenerate(NA,vk0,vsk,r_Se,r_Sm,r_Me,r_Mm,z_scan_un,theta_array,d_gra(ii),system_pol);%生成CSI信号
+    signal_gra = signal_gra(:);
+    signal_gra = signal_gra-mean(signal_gra,'omitnan'); %去掉直流
+    cost(ii) = sum(abs(signal-signal_gra).^2,'all','omitnan');%计算误差
+end
+toc;
+
+figure();
+plot(d_gra,cost,'LineWidth',1.5,'Color',Color(1,:));
+defaultAxes(2);
+xlabel('d/$\mu$ m','Interpreter','latex');
+
+[~,min_index] = min(cost); %误差最小值对应的索引
+d_pre = d_gra(min_index);
+cost_pre = cost(min_index);
+
+if min_index == 1 || min_index == length(d_gra)
+    warning('误差最小值位于搜索边界，建议扩大 搜索范围');
+else
+    %抛物线三点拟合
+    % 最小值左右相邻的三个代价函数值
+    cost_left   = cost(min_index-1);
+    cost_center = cost(min_index);
+    cost_right  = cost(min_index+1);
+    
+    denominator = cost_left - 2*cost_center + cost_right;
+    
+    % denominator>0 表示局部抛物线开口向上
+    if isfinite(denominator) && denominator > eps(max(abs(cost)))
+        delta_index = 0.5 * ...
+            (cost_left - cost_right) / denominator;
+        
+        % 顶点原则上应该落在相邻两个采样点之间
+        if abs(delta_index) <= 1
+            d_pre = d_gra(min_index) + delta_index*d_peri;
+            
+            % 抛物线顶点对应的代价值
+            cost_pre = cost_center ...
+                - (cost_left-cost_right)^2/(8*denominator);
+        else
+            warning('抛物线顶点超出相邻采样区间，使用离散最小值。');
+        end
+    else
+        warning('局部代价函数不满足开口向上的抛物线条件，使用离散最小值。');
+    end
+end
+
+hold on;
+scatter(d_pre,cost_pre,25,Color(2,:),'filled');
+hold off;
+
+disp(['细网格拟合法:',num2str(d_pre)]);
